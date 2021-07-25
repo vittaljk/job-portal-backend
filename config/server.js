173c98968d843933c0eb2f84b8ee3c0901283397
vijay.vittal@hashouttech.com <vijay.vittal@hashouttech.com>
2021-07-25 14:31:53 +0530

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '23423b16ffe64ed48fc173d2065a7890'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '20071db2-f4fc-492b-aa92-1c3aed13f514'
+};
